25da0d43ee18f75c56a2ab798e7604de49dc1a48
pngcrypt <png.crypt@yandex.ru>
2016-12-04 04:59:43 +0300

    ref

--- Brchan_Colorized_ID.user.js
+++ Brchan_Colorized_ID.user.js
@@ -15,12 +15,8 @@
 	if(!window.jQuery || !window.$) return;
 	var $ = window.$;
 
-	var ids = [],
-		stylesheet,
-		HLtype = 2, // Type of highlight: 0 - off, 1 - ID to font color; 2 - ID to background color
+	var HLtype = 2, // Type of highlight: 0 - off, 1 - ID to font color; 2 - ID to background color
 		lightLimit = 160, // threshold of brightness
-		selectedID = '',
-		selectedCNT = 0,
 		
 		// hl-type: 1
 		bgDark = "#404040", // color of BG when font color is bright
@@ -28,97 +24,78 @@
 
 		// hl-type: 2
 		colDark = "#000000", // color of font when BG color is bright
-		colLight = "#ffffff"; // color of font when BG color is dark
+		colLight = "#ffffff", // color of font when BG color is dark
 
-	function addStyle(selector, rules) {
-		if(!stylesheet) {
-			stylesheet = document.styleSheets[0];
-			if(!stylesheet) return;
-		}
-		if("insertRule" in stylesheet)
-			stylesheet.insertRule(selector + "{" + rules + "}", 0);
-		else
-			stylesheet.addRule(selector, rules, 0);
-	}
+		$thread, selectedID = '', selectedCNT, selectedKey;
 
-	function ColorizeID(parent) {
-		var id, rgb=[], light;
-		$(parent).find('span.poster_id').each(function(){
-			id = this.textContent;
-			if(HLtype && (rgb = id.match(/(..)(..)(..)/))) {
-				rgb[0] = parseInt(rgb[1], 16);
-				rgb[1] = parseInt(rgb[2], 16);
-				rgb[2] = parseInt(rgb[3], 16);
-				light = 0.2126*rgb[0] + 0.7152*rgb[1] + 0.0722*rgb[2]; // color brightness
 
-				switch(HLtype) {
-					case 1:
-						this.style.color = "#"+id;
-						this.style.backgroundColor = light > lightLimit ? bgDark : bgLight;
-						break;
+    $('head').append('<style>' +
+		'span.poster_id {border-radius: 4px; font-size: 0.9em; padding: 2px; font-weight: normal;}' +
+		// '.bci_selected {background-blend-mode: hard-light !important; background-color: #ff0000 !important;}' +
+		'.bci_selected {background-blend-mode: luminosity !important; background-color: rgba(255, 0, 0, 0.25) !important; border-color: #ff0000 !important;}' +
+		'.bci_hidden > div {display:none !important;}' +
+		'.bci_hidden .poster_id {opacity: 0.6; !important;}' +
+        '</style>');
 
-					case 2:
-						this.style.backgroundColor = "#"+id;
-						this.style.color = light > lightLimit ? colDark : colLight;
-						break;
-				}
+	var ColorizeID = function($post) {
+		var $pid = $post.find('.poster_id'),
+			id = $pid.text();
+		if(id == selectedID)
+			$post.addClass('bci_selected');
+
+		if(HLtype) {
+			var rgb = [parseInt(id.substr(0,2), 16), parseInt(id.substr(2,2), 16), parseInt(id.substr(4,2), 16)],
+				light = (Math.max.apply(null,rgb) + Math.min.apply(null,rgb)) / 2;
+			switch(HLtype) {
+				case 1: $pid.css({'color': '#'+id, 'backgroundColor': (light > lightLimit ? bgDark : bgLight)}); break;
+				case 2: $pid.css({'backgroundColor': '#'+id, 'color': (light > lightLimit ? colDark : colLight)}); break;
 			}
-			if(selectedID == id && this.parentElement && this.parentElement.parentElement) {
-				$(this.parentElement.parentElement).addClass('bci_selected');
-			}
-		});
-	}
-
-	function updateCounter() {
-		if(selectedID === '') return;
-		var cnt = 1;
-		selectedCNT = $('div.thread .bci_selected').length;
-		$('div.thread').find('.bci_selected').each(function(){
-			$('span.poster_id', this).attr('title', cnt + ' ('+selectedCNT+')');
-			cnt++;
-		});
-	}
-
-	function onIDclick() {
-		var parent,
-			self = this,
-			id = self.textContent;
-
-		if(!(parent = self.parentElement)) return; 
-		if(!(parent = parent.parentElement)) return; // post
-		if(!(parent = parent.parentElement)) return; // thread
-		$(parent).find('div.post').each(function(){
-			$(this).removeClass('bci_selected');
-			$('span.poster_id', this).attr('title', '');
-		});
-		if(id == selectedID) {
-			selectedID = '';
-			selectedCNT = 0;
 		}
-		else {
-			selectedID = id;
-			$(parent).find('span.poster_id').each(function(){
-				if(this.textContent !== id) return;
-				$(this.parentElement.parentElement).addClass('bci_selected');
-			});
-			updateCounter();
-		}
-	}
-
-	function onNewPost(e, el) {
-		ColorizeID(el);
-		updateCounter();
 	};
 
-	console.log('ColorizeID started');
+	var updateCounter = function() {
+		if(selectedID === '' || !$thread) return;
+		var cnt = 1,
+			$selected = $thread.find('.bci_selected');
+		selectedCNT = $selected.length;
+		$selected.find('.poster_id').each(function() {
+			$(this).attr('title', cnt + ' (' + selectedCNT + ')');
+			cnt++;
+		});
+	};
 
-	// style of id
-	addStyle('span.poster_id', 'border-radius: 4px; font-size: 0.9em; padding: 2px; font-weight: bold; ');
-	addStyle('div.bci_selected', 'background-blend-mode: hard-light !important; background-color: #ff0000 !important;');
-	//background-blend-mode: hard-light; background-color: #b3569c;
+	$('div.thread').on('click', 'span.poster_id', function(ev) {
+		$thread = $(this).parents('.thread');
+		var id = $(this).text();
+		var $posts = $thread.find('.post').removeClass('bci_selected bci_hidden').removeAttr('title');
 
-	ColorizeID(document);
-	$(document).on('new_post', onNewPost);
-	$('div.thread').on('click', 'span.poster_id', onIDclick);
+		if(id == selectedID)
+			selectedID = '';
+		else {
+			selectedID = id;
+			if(ev.ctrlKey) {
+				$posts.find('.poster_id:not(:contains("' + id + '"))')
+					.parents('.post')
+					.addClass('bci_hidden');
+			}
+			else {
+				$posts.find('.poster_id:contains("' + id + '")')
+					.parents('.post')
+					.addClass('bci_selected');
+			}
+		}
+		updateCounter();
+	});
+
+	$(document).on('new_post', function(ev, el) {
+		ColorizeID($(el));
+		updateCounter();
+	});
+
+	$('div.thread .post').each(function(idx, el) {
+		ColorizeID($(el));
+	});
+
+	console.log('BRchan ColorizedID started');
 
 })();
